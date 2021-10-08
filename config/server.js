9bd06bc01c6a959002c888c15eea2c8179b1e8fa
zakywtf <novazaky1@gmail.com>
2021-10-08 10:27:28 +0700

    Feat: Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86605b0c5c13482175dfeac3d81b3415'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '99b4d5f3-170e-402e-8313-f8a960164383'
+};
